82be5ec140e59bae5779bab1a2fe0d58209fc2c3
Anubhav Ghosh <iamanubhav11@gmail.com>
2025-01-08 00:36:11 +0530

    Added Reverse Schema

---
+++ src/models/reserve_model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const reservationSchema = new mongoose.Schema({
+  gift: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Gift',
+    required: true,
+  },
+  reservedBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['reserved', 'purchased'],
+    default: 'reserved',
+  },
+  reservedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export const Reservation = mongoose.model('Reservation', reservationSchema);
